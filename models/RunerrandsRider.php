a4fd7627331db3a0b6123aa1c0c962fddd028226
bricksasp <649909457@qq.com>
2021-02-13 09:09:59 +0800

    client端

--- models/RunerrandsRider.php
+++ models/RunerrandsRider.php
@@ -3,6 +3,7 @@
 namespace bricksasp\models;
 
 use Yii;
+use bricksasp\base\Tools;
 
 /**
  * This is the model class for table "basp_runerrands_rider".
@@ -13,7 +14,8 @@ use Yii;
  * @property string|null $name
  * @property string|null $phone
  * @property int|null $has_car
- * @property int|null $status
+ * @property int|null $status 1通过2拒绝
+ * @property string|null $refuse_reasons
  * @property int|null $created_at
  * @property int|null $updated_at
  */
@@ -27,15 +29,27 @@ class RunerrandsRider extends \bricksasp\base\BaseActiveRecord
         return 'basp_runerrands_rider';
     }
 
+    public function behaviors()
+    {
+        return [
+            \yii\behaviors\TimestampBehavior::className(),
+        ];
+    }
+
     /**
      * {@inheritdoc}
      */
     public function rules()
     {
         return [
-            [['user_id', 'school_id', 'school_area_id', 'has_car', 'status', 'created_at', 'updated_at'], 'integer'],
+            [['school_id', 'name', 'phone', 'has_car', 'password'], 'required'],
+            [['owner_id', 'user_id', 'school_id', 'school_area_id', 'has_car', 'status', 'created_at', 'updated_at'], 'integer'],
             [['name'], 'string', 'max' => 8],
+            [['password'], 'string', 'max' => 32],
             [['phone'], 'string', 'max' => 11],
+            [['refuse_reasons'], 'string'],
+            [['user_id'], 'unique', 'message' => '请勿重复申请'],
+            [['password'], 'checkPassword'],
         ];
     }
 
@@ -48,12 +62,29 @@ class RunerrandsRider extends \bricksasp\base\BaseActiveRecord
             'user_id' => 'User ID',
             'school_id' => 'School ID',
             'school_area_id' => 'School Area ID',
-            'name' => 'Name',
-            'phone' => 'Phone',
-            'has_car' => 'Has Car',
+            'name' => '姓名',
+            'phone' => '电话',
+            'has_car' => '有无电瓶车',
+            'password' => '密码',
             'status' => 'Status',
             'created_at' => 'Created At',
             'updated_at' => 'Updated At',
         ];
+    }
+
+    public function checkPassword()
+    {
+        $this->password = md5($this->password);
+    }
+
+    public function formatData($data)
+    {
+        $data = parent::formatData($data);
+        $sc = StoreRelation::find()->where(['object_id'=>$data['school_id']??null,'type'=>StoreRelation::TYPE_SCHOOL])->one();
+        if (!$sc) {
+            Tools::breakOff('学校未开放');
+        }
+        $data['owner_id'] = $sc->owner_id;
+        return $data;
     }
 }
---
+++ runerrands/controllers/RiderController.php
@@ -0,0 +1,237 @@
+<?php
+
+namespace bricksasp\runerrands\controllers;
+
+use Yii;
+use bricksasp\base\Tools;
+use bricksasp\models\RunerrandsRider;
+use yii\data\ActiveDataProvider;
+
+class RiderController extends \bricksasp\base\BackendController
+{
+	public function noLoginAction()
+	{
+		return [
+			'index',
+		];
+	}
+
+    public function loginAction()
+    {
+        return [
+            'view',
+            'create',
+            'update',
+        ];
+    }
+
+    /**
+     * @OA\Get(path="/runerrands/rider/index",
+     *   summary="骑手列表",
+     *   tags={"跑腿模块"},
+     *   @OA\Parameter(name="access-token",in="header",@OA\Schema(type="string"),description="用户请求token"),
+     *   
+     *   @OA\Parameter(name="page",in="query",@OA\Schema(type="integer"),description="当前叶数"),
+     *   @OA\Parameter(name="pageSize",in="query",@OA\Schema(type="integer"),description="每页行数"),
+     *   @OA\Parameter(name="school_id",in="query",@OA\Schema(type="integer"),description="学校id"),
+     *   
+     *   @OA\Response(
+     *     response=200,
+     *     description="返回数据",
+     *     @OA\MediaType(
+     *         mediaType="application/json",
+     *         @OA\Schema(ref="#/components/schemas/pagination"),
+     *     ),
+     *   ),
+     * )
+     */
+    public function actionIndex()
+    {
+        $params = Yii::$app->request->get();
+        $query =  RunerrandsRider::find();
+        $query->andFilterWhere($this->ownerCondition());
+        $query->andFilterWhere(['school_id'=>$params['school_id']??Tools::breakOff('未找学校相应到数据')]);
+        $query->andFilterWhere(['like', 'name', $params['name']??null]);
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        return $this->success([
+          'list' => $dataProvider->models,
+          'pageCount' => $dataProvider->pagination->pageCount,
+          'totalCount' => $dataProvider->pagination->totalCount,
+          'page' => $dataProvider->pagination->page + 1,
+          'pageSize' => $dataProvider->pagination->limit,
+        ]);
+    }
+
+    /**
+     * @OA\Get(path="/runerrands/rider/view",
+     *   summary="骑手详情",
+     *   tags={"跑腿模块"},
+     *   
+     *   @OA\Parameter(name="id",in="query",@OA\Schema(type="integer"),description="id"),
+     *   
+     *   @OA\Response(
+     *     response=200,
+     *     description="返回数据",
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       
+     *       @OA\Schema(ref="#/components/schemas/RunerrandsRiderUpdate"),
+     *     ),
+     *   ),
+     * )
+     */
+    public function actionView()
+    {
+        $params = Yii::$app->request->get();
+        $model = $this->findModel(['user_id'=>$params['id'] ?? $this->current_user_id]);
+        return $this->success($model);
+    }
+
+    /**
+     * @OA\Post(path="/runerrands/rider/create",
+     *   summary="创建骑手",
+     *   tags={"跑腿模块"},
+     *   @OA\Parameter(name="access-token",in="header",@OA\Schema(type="string"),description="用户请求token"),
+     *   
+     *   @OA\RequestBody(
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         ref="#/components/schemas/RunerrandsRiderCreate"
+     *       )
+     *     )
+     *   ),
+     *   
+     *   @OA\Response(
+     *     response=200,
+     *     description="返回数据",
+     *     @OA\MediaType(
+     *         mediaType="application/json",
+     *         @OA\Schema(ref="#/components/schemas/response"),
+     *     ),
+     *   ),
+     * )
+     *
+     * @OA\Schema(
+     *   schema="RunerrandsRiderCreate",
+     *   description="骑手",
+     *   @OA\Property(property="school_id", type="integer", description=""),
+     *   @OA\Property(property="school_area_id", type="integer", description=""),
+     *   @OA\Property(property="name", type="string", description="姓名"),
+     *   @OA\Property(property="phone", type="integer", description="",),
+     *   @OA\Property(property="has_car", type="integer", description="1有车",),
+     *   @OA\Property(property="status", type="integer", description="",),
+     *   @OA\Property(property="refuse_reasons", type="string", description="",),
+     *   required={"name"}
+     * )
+     */
+    public function actionCreate()
+    {
+        $params = $this->queryMapPost();
+        $model = new RunerrandsRider();
+        if ($model->saveData($params)) {
+            return $this->success();
+        }
+
+        return $this->fail($model->errors);
+    }
+
+    /**
+     * @OA\Post(path="/runerrands/rider/update",
+     *   summary="修改骑手",
+     *   tags={"跑腿模块"},
+     *   @OA\Parameter(name="access-token",in="header",@OA\Schema(type="string"),description="用户请求token"),
+     *   
+     *   @OA\RequestBody(
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         ref="#/components/schemas/RunerrandsRiderUpdate"
+     *       )
+     *     )
+     *   ),
+     *   
+     *   @OA\Response(
+     *     response=200,
+     *     description="返回数据",
+     *     @OA\MediaType(
+     *         mediaType="application/json",
+     *         @OA\Schema(ref="#/components/schemas/response"),
+     *     ),
+     *   ),
+     * )
+     * 
+     * 
+     * @OA\Schema(
+     *   schema="RunerrandsRiderUpdate",
+     *   description="骑手数据",
+     *   allOf={
+     *     @OA\Schema(ref="#/components/schemas/RunerrandsRiderCreate"),
+     *   }
+     * )
+     */
+    public function actionUpdate()
+    {
+        $params = $this->queryMapPost();
+        $model = $this->findModel(['user_id'=>$params['id'] ?? $this->current_user_id]);
+
+        if ($model->saveData($params)) {
+            return $this->success();
+        }
+
+        return $this->fail($model->errors);
+    }
+
+    /**
+     * @OA\Post(path="/runerrands/rider/delete",
+     *   summary="删除骑手",
+     *   tags={"跑腿模块"},
+     *   
+     *   @OA\Parameter(name="access-token",in="header",@OA\Schema(type="string"),description="用户请求token"),
+     *   
+     *   @OA\RequestBody(
+     *     @OA\MediaType(
+     *       mediaType="application/json",
+     *       @OA\Schema(
+     *         @OA\Property(property="ids", type="array", description="ids", @OA\Items()),
+     *       )
+     *     )
+     *   ),
+     *   
+     *   @OA\Response(
+     *     response=200,
+     *     description="返回数据",
+     *     @OA\MediaType(
+     *         mediaType="application/json",
+     *         @OA\Schema(ref="#/components/schemas/response"),
+     *     ),
+     *   ),
+     * )
+     *
+     * 
+     */
+    public function actionDelete()
+    {
+        $params = $this->queryMapPost();
+        return RunerrandsRider::deleteAll(['id'=>$params['ids']??0]) ? $this->success() : $this->fail();
+    }
+
+    /**
+     * Finds the RunerrandsRider model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return RunerrandsRider the loaded model
+     */
+    protected function findModel($id)
+    {
+        if (($model = RunerrandsRider::findOne($id)) !== null) {
+            return $model;
+        }
+
+        Tools::breakOff(40001);
+    }
+}
